ea713af9f3aebcd1fbb8cdfa9d5957ce78f0b058
ControlNet <smczx@hotmail.com>
2023-02-12 11:23:39 +1100

    fix ui test

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { act } from "react-dom/test-utils";
 
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./stores/store";
 
 globalThis.IS_REACT_ACT_ENVIRONMENT = true;
 
@@ -10,7 +12,11 @@ const render = () => {
   act(() => {
     const div = document.createElement("div");
     const root = createRoot(div);
-    root.render(<App />);
+    root.render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
   });
 };
 
